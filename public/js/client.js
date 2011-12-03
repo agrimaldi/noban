6a76881e65bdcb4071ca0fa42109cd563136c58e
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2011-12-04 00:11:47 +0100

    Merged develop/client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,54 +1,70 @@
+var joinGame
+  , leaveGame
+  , displayAvailableGames
+  , addAvailableGame;
+
 $(document).ready(function () {
-var games = io.connect('/games')
-  , game = io.connect('/game')
 
-var joinGame = function(gameId) {
-  game.emit('joingame', gameId);
-};
+  var games = io.connect('/games')
+    , game = io.connect('/game')
 
-var leaveGame = function(gameId) {
-  game.emit('leavegame', gameId);
-};
 
-var displayAvailableGames = function(games) {
-  $('#games_available').empty();
-  games.forEach(function(game) {
-    addAvailableGame(game);
+  //games
+  
+  games.on('games:list', function(data) {
+    displayAvailableGames(data, game);
+  }); 
+
+  displayAvailableGames = function(games) {
+    $('#games_available').empty();
+    games.forEach(function(game) {
+      addAvailableGame(game);
+    }); 
+  };  
+
+  addAvailableGame = function(game) {
+    $('#games_available').append(
+      '<div>'+                                                                                           
+        game.title+
+        '<input type="button" value="Join" onClick="joinGame(\''+game._id+'\')">'+
+      '</div>'
+    );  
+  };  
+
+
+  //game
+
+  joinGame = function(gameId) {
+    game.emit('game:join', gameId);
+  };  
+
+  leaveGame = function(gameId) {
+    game.emit('game:leave', gameId);
+  };  
+
+  game.on('game:joined', function(msg) {
+    console.log(msg);
+  }); 
+
+  game.on('players:list', function(data) {
+    console.log('players list :')
+    console.log(data);
+  }); 
+
+
+  //events
+
+  $('#leave_button').click(function(e) {
+    console.log('leaving');
+    socket.emit('leave_game');
   });
-};
 
-var addAvailableGame = function(game) {
-  $('#games_available').append(
-    '<div>'+
-      game.title+
-      '<input type="button" value="Join" onClick="joinGame(\''+game._id+'\')">'+
-    '</div>'
-  );
-};
-
-games.on('games', function(data) {
-  displayAvailableGames(data, game);
-});
-
-game.on('gamejoined', function(msg) {
-  console.log(msg);
-});
-
-game.on('players_list', function(data) {
-  console.log('players list :')
-  console.log(data);
-});
-
-$('#leave_button').click(function(e) {
-  console.log('leaving');
-  socket.emit('leave_game');
-});
-
-$('#create_game_form').submit(function(e) {
-  e.preventDefault();
-  games.emit('newgame', {
-    title: $('#title').val()
-  , size: $('#size').val()
+  $('#create_game_form').submit(function(e) {
+    e.preventDefault();
+    games.emit('game:new', {
+      title: $('#title').val()
+    , size: $('#size').val()
+    });
   });
-});
+
 });
